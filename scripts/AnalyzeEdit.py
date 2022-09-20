45d0002daf6bb35a725f34eefafefbec0384970d
dominik504 <dominik.herler@uni-graz.at>
2022-09-20 17:23:51 +0200

    added Betaversion to work with datetime

--- scripts/AnalyzeEdit.py
+++ scripts/AnalyzeEdit.py
@@ -31,54 +31,61 @@ def read_result(path, parameter):
     result["parameter_area"] = result[f"{parameter}"] / result.Area
     return result
 
-def add_index(path, parameter, index_file):
-    """
-    Using the 'read_result' function and adding a field ID and datetime to the 
-    results. Therefore a .csv file is needed
 
-    Parameters
-    ----------
-    path : String
-        Directory where the .csv files for all timesteps are. 
-        They must be exported from paraview.
-    parameter : String
-        The Parameter which should be analyzed and plotted.
-    index_file : String
-        Directory where the index file is located. It must be a .csv file without
-        header and tapstop seperated. 3 columns with datetime, the ID (first active
-        measurment = 1, before its NaN) and one column with 0 (ERT not active) or 
-        1 (ERT active).
+path="C:/Users/domin/OneDrive/Desktop/masterarbeit/Tanneben/20220705_Daten/messung1_teil1_2_3_4_5/ergebnisse_teil1_2_3/slices"
+parameter="Resistivity(ohm.m)"
+index_file="C:/Users/domin/OneDrive/Desktop/masterarbeit/Tanneben/ERT_aktiv.csv"
+#!! TO DO: wenn letzte Zeile aktiv funzt für Juni für Juli verwechselts aber Month & Day
+# erstmal nur für ID weiterhin
+# def add_index(path, parameter, index_file):
+    # """
+    # Using the 'read_result' function and adding a field ID and datetime to the 
+    # results. Therefore a .csv file is needed
 
-    Returns
-    -------
-    result_date : Dataframe
-        A Pandas Dataframe same as from function 'read_results' containing the ID
-        and Datetime.
+    # Parameters
+    # ----------
+    # path : String
+    #     Directory where the .csv files for all timesteps are. 
+    #     They must be exported from paraview.
+    # parameter : String
+    #     The Parameter which should be analyzed and plotted.
+    # index_file : String
+    #     Directory where the index file is located. It must be a .csv file without
+    #     header and tapstop seperated. 3 columns with datetime, the ID (first active
+    #     measurment = 1, before its NaN) and one column with 0 (ERT not active) or 
+    #     1 (ERT active).
 
-    """
-    # read and name index file
-    active = pd.read_csv(index_file, sep="\t", header=None)
-    active.columns = ["datetime", "ID", "active"]
-    
-    # extract all columns where measurment were active
-    extraction = active[active.active==1]
-    extraction.reset_index(drop=True, inplace=True)
-    
-    # read paraview results using 'read_result' function
-    result = read_result(path=path, parameter=parameter)
-    result.reset_index(drop=True, inplace=True)
+    # Returns
+    # -------
+    # result_date : Dataframe
+    #     A Pandas Dataframe same as from function 'read_results' containing the ID
+    #     and Datetime.
 
-    # check if index file and actual results have the same length of active ERTs
-    if len(extraction) != len(result):
-        raise IndexError("Different lengths of index_file and results \nCheck if there are as many active (=1) lines in index_file column 3 as result files")
-        sys.exit()
-    
-    # concat datetime extraction to paraview results
-    concat = pd.concat([extraction,result], axis=1)
-    concat = concat.set_index("ID").drop("datetime", axis=1)
+    # """
+# read and name index file
+active = pd.read_csv(index_file, sep="\t", header=None)
+active.columns = ["datetime", "ID", "active"]
 
-    # concat extraction with rest of index file to get the whole timerange
-    active = active.set_index("ID").drop("active", axis=1)
-    result_date = pd.concat([active, concat], axis=1)
-    result_date = result_date.set_index("datetime")
-    return result_date
+# extract all columns where measurment were active
+extraction = active[active.active==1]
+extraction.reset_index(drop=True, inplace=True)
+
+# read paraview results using 'read_result' function
+result = read_result(path=path, parameter=parameter)
+result.reset_index(drop=True, inplace=True)
+
+# check if index file and actual results have the same length of active ERTs
+if len(extraction) != len(result):
+    raise IndexError("Different lengths of index_file and results \nCheck if there are as many active (=1) lines in index_file column 3 as result files")
+    sys.exit()
+
+# concat datetime extraction to paraview results
+concat = pd.concat([extraction,result], axis=1)
+concat = concat.set_index("ID").drop("datetime", axis=1)
+
+# concat extraction with rest of index file to get the whole timerange
+active = active.set_index("ID").drop("active", axis=1)
+result_date = pd.concat([active, concat], axis=1)
+result_date = result_date.set_index("datetime")
+result_date.index = pd.to_datetime(result_date.index, format="%d.%m.%Y %H:%M")
+    # return result_date
--- scripts/getianalyze.py
+++ scripts/getianalyze.py
@@ -1,5 +1,6 @@
 import argparse
 import matplotlib.pyplot as plt
+import datetime
 
 from . import AnalyzeEdit
 from . import AnalyzePlot
@@ -30,6 +31,25 @@ def analyze():
     index_file = args.index_file
     area = args.area
     save = args.save
+    
+    # parsing min and max ID or Datetime and check for errors
+    try: # if min and max were parsed as ID
+        min_index = int(args.min_max[0])
+        max_index = int(args.min_max[1])
+    except ValueError:
+        try: # if min and max were parsed as date without time
+            datetime.datetime.strptime(args.min_max[0], '%Y-%m-%d')
+            datetime.datetime.strptime(args.min_max[1], '%Y-%m-%d')
+            min_index = args.min_max[0]
+            max_index = args.min_max[1]
+        except ValueError:
+            try: # if min and max were parsed as date with time
+                datetime.datetime.strptime(args.min_max[0], '%Y-%m-%d %H:%M')
+                datetime.datetime.strptime(args.min_max[1], '%Y-%m-%d %H:%M')
+                min_index = args.min_max[0]
+                max_index = args.min_max[1]
+            except ValueError:
+                raise ValueError("Incorrect data format, should be Year-month-day or Year-month-day Hour:Minute")
     
     if  index_file != None:
         index = True 
